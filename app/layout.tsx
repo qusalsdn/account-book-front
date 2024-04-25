76fab9971b22aca0055ca9afd5973448b390acd9
변민우 <yja05092@gmail.com>
2024-04-25 15:06:32 +0900

    Suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "가계부",
@@ -12,8 +13,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className="w-[480px] mx-auto h-full my-10 px-5 select-none font-bold">{children}</body>
-    </html>
+    <Suspense>
+      <html lang="ko">
+        <body className="w-[480px] mx-auto h-full my-10 px-5 select-none font-bold">
+          {children}
+        </body>
+      </html>
+    </Suspense>
   );
 }
--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -15,7 +15,7 @@ export default function SignUp() {
   useEffect(() => {
     axios
       .post(
-        "http://43.202.3.171:3000/auth",
+        "http://43.203.200.5:3000/auth",
         { username: Cookies.get("accessToken") },
         { headers: { Authorization: `Bearer ${accessToken}` } }
       )
@@ -30,7 +30,7 @@ export default function SignUp() {
 
   const onSubmit = (formData: any) => {
     axios
-      .post("http://43.202.3.171:3000/auth/signUp", formData)
+      .post("http://43.203.200.5:3000/auth/signUp", formData)
       .then((res) => {
         if (res.data.ok) router.replace("/signIn");
         else console.error(res.data);
